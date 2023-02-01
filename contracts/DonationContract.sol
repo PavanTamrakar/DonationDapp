076876feddd9a05623712669952c959467ac88d8
PavanTamrakar <thepavantamrakar@gmail.com>
2023-02-01 12:34:46 +0530

    setup

---
+++ contracts/DonationContract.sol
@@ -0,0 +1,17 @@
+//SPDX-Licence-Identifier: GPL-3.0
+pragma solidity ^0.8.10;
+
+contract DonationContract {
+    uint256 public totalOrg = 0;
+
+    struct Org{
+        uint256 id;
+        string hash;
+        string description;
+        uint256 donationAmount;
+        address payable author;
+    }
+
+    
+
+}
